e423a1554bd4f90dd15382f67396c747bec2b3f2
alinapak <alina.pakamoryte@gmail.com>
2022-04-26 17:29:03 +0300

    Changed previous local storage function and created readItems function. Now when user refreshes browser, you can still see shopping list on a browser. READ commit

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ let shoppingList = document.getElementById("shopping-list");
 
 shoppingButton.addEventListener("click", addShoppingInput);
 shoppingList.addEventListener("click", deleteOrGet);
+document.addEventListener("DOMContentLoaded", readItems);
 
 function addShoppingInput(event) {
     event.preventDefault();
@@ -39,13 +40,38 @@ function deleteOrGet(event) {
         shoppingItem.classList.toggle("in-a-cart");
     }
 }
-function saveLocalItems(newItem) {
+function saveLocalItems(items) {
     let itemList;
     if (localStorage.getItem("itemList") === null) {
         itemList = [];
     } else {
         itemList = JSON.parse(localStorage.getItem("itemList"));
     }
-    itemList.push(newItem);
+    itemList.push(items);
     localStorage.setItem("itemList", JSON.stringify(itemList));
+}
+function readItems() {
+    let itemList;
+    if (localStorage.getItem("itemList") === null) {
+        itemList = [];
+    } else {
+        itemList = JSON.parse(localStorage.getItem("itemList"));
+    }
+    itemList.forEach(function (items) {
+        let list = document.createElement("div");
+        list.classList.add("list");
+        let newItem = document.createElement("li");
+        newItem.innerText = items;
+        newItem.classList.add("list-item");
+        list.appendChild(newItem);
+        let gotItButton = document.createElement("button");
+        gotItButton.innerText = "Got It";
+        gotItButton.classList.add("got-it");
+        list.appendChild(gotItButton);
+        let deleteButton = document.createElement("button");
+        deleteButton.innerText = "Delete";
+        deleteButton.classList.add("delete");
+        list.appendChild(deleteButton);
+        shoppingList.appendChild(list);
+    })
 }
