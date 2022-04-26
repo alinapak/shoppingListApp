ff2ed510cd929f671956fc1965d76acab11add53
alinapak <alina.pakamoryte@gmail.com>
2022-04-26 15:31:22 +0300

    Added local storage to Shopping List app, now all inputs are saved in local stoarge, its is CREATE commit

--- index.js
+++ index.js
@@ -6,35 +6,46 @@ shoppingButton.addEventListener("click", addShoppingInput);
 shoppingList.addEventListener("click", deleteOrGet);
 
 function addShoppingInput(event) {
-   event.preventDefault();
-   let list = document.createElement("div");
-   list.classList.add("list");
-   let newItem = document.createElement("li");
-   newItem.innerText = shoppingInput.value;
-   if (shoppingInput.value.length!==0) {
-      newItem.classList.add("list-item");
-      list.appendChild(newItem);
-      let gotItButton = document.createElement("button");
-      gotItButton.innerText = "Got It";
-      gotItButton.classList.add("got-it");
-      list.appendChild(gotItButton);
-      let deleteButton = document.createElement("button");
-      deleteButton.innerText = "Delete";
-      deleteButton.classList.add("delete");
-      list.appendChild(deleteButton);
-      shoppingList.appendChild(list);
-      shoppingInput.value = "";
-   }
+    event.preventDefault();
+    let list = document.createElement("div");
+    list.classList.add("list");
+    let newItem = document.createElement("li");
+    newItem.innerText = shoppingInput.value;
+    if (shoppingInput.value.length !== 0) {
+        newItem.classList.add("list-item");
+        list.appendChild(newItem);
+        saveLocalItems(shoppingInput.value);
+        let gotItButton = document.createElement("button");
+        gotItButton.innerText = "Got It";
+        gotItButton.classList.add("got-it");
+        list.appendChild(gotItButton);
+        let deleteButton = document.createElement("button");
+        deleteButton.innerText = "Delete";
+        deleteButton.classList.add("delete");
+        list.appendChild(deleteButton);
+        shoppingList.appendChild(list);
+        shoppingInput.value = "";
+    }
 }
 
 function deleteOrGet(event) {
-   let item = event.target;
-   if (item.classList[0] === "delete") {
-      let shoppingItem = item.parentElement;
-      shoppingItem.remove();
-   }
-   else if (item.classList[0] === "got-it") {
-      let shoppingItem = item.parentElement;
-      shoppingItem.classList.toggle("in-a-cart");
-   }
+    let item = event.target;
+    if (item.classList[0] === "delete") {
+        let shoppingItem = item.parentElement;
+        shoppingItem.remove();
+    }
+    else if (item.classList[0] === "got-it") {
+        let shoppingItem = item.parentElement;
+        shoppingItem.classList.toggle("in-a-cart");
+    }
+}
+function saveLocalItems(newItem) {
+    let itemList;
+    if (localStorage.getItem("itemList") === null) {
+        itemList = [];
+    } else {
+        itemList = JSON.parse(localStorage.getItem("itemList"));
+    }
+    itemList.push(newItem);
+    localStorage.setItem("itemList", JSON.stringify(itemList));
 }
